e0fa95a91159f7649a028ee529d804d3e35ac980
Matheus Pereira <empregos97@gmail.com>
2024-01-23 09:57:24 -0300

    reviewing the documentation on gemini ia and working on a new nextjs architecture that has pages & app directory

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import { GetServerSideProps } from "next";
+
+export default function Home({ hello }: { hello: string }) {
+  return <div className="bg-red-200">{hello}</div>;
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    props: {
+      hello: "ok",
+    },
+  };
+};
