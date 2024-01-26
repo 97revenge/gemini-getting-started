91609d127c96caf5ea70ccd3f6c3b3228aa2b7a9
Matheus Pereira <empregos97@gmail.com>
2024-01-26 14:52:54 -0300

    fixing api key error

--- app/chat/route.ts
+++ app/chat/route.ts
@@ -6,17 +6,6 @@ import { cookies } from "next/headers";
 
 export async function GET(req: Request) {
   const prompt = "hey you you doing  ? ";
-  const result = await model.generateContent(prompt);
 
-  const response = await result.response;
-
-  const text = response.text();
-
-  const instance = await jwt.sign(text, String(process.env.JWT_TOKEN));
-
-  const cookieStore = cookies();
-
-  cookieStore.set("chat", instance);
-
-  return Response.json(text);
+  return Response.json(prompt);
 }
