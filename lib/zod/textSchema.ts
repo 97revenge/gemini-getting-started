c80f6e75051de9d8e71d91a183750424d08b1ef8
Matheus Pereira <empregos97@gmail.com>
2024-02-05 11:43:20 -0300

    maintaining the text validation status with a minimum of 30 characters if in this case it is not optional (when it has at least 1 character, 30 characters are necessary to be thruly)

--- lib/zod/textSchema.ts
+++ lib/zod/textSchema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const textSchema = z.object({
-  title: z.string().optional(),
+  title: z.string().min(30).optional(),
   text: z.string().min(500).max(4000, {
     message: "Sua redação precisa pode ter no maximo 4000 caracteres  ",
   }),
